8ce14fc262132ac46f0c3d8f1b47c87858888fdf
mrscrew <mr_screw@mail.ru>
2024-09-05 12:31:07 +0300

    .

--- flake.nix
+++ flake.nix
@@ -2,56 +2,42 @@
   description = "My system configuration";
 
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
-
-    let
-      system = "x86_64-linux";
-    in
-    {
-      # Конфигурации хостов
-      nixosConfigurations = {
-        # nixos-master
-        nixos-master = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            pkgs-stable = import nixpkgs-stable {
-              inherit system;
-              config.allowUnfree = true;
-            };
-            inherit inputs system;
-          };
-          modules = [
-            ./nixos/configuration.nix
-          ];
-        };
-        # nixos-mama
-        nixos-mama = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            pkgs-stable = import nixpkgs-stable {
-              inherit system;
-              config.allowUnfree = true;
-            };
-            inherit inputs system;
-          };
-          modules = [
-            ./nixos/configuration.nix
-          ];
-        };
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }: let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations = {
+      nixos-master = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration-nixos-master.nix
+          { pkgs-stable = import nixpkgs-stable { inherit system; config.allowUnfree = true; }; }
+        ];
       };
-
-      homeConfigurations.master = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ ./home-manager/home.nix ];
+      nixos-mama = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration-nixos-mama.nix
+          { pkgs-stable = import nixpkgs-stable { inherit system; config.allowUnfree = true; }; }
+        ];
       };
     };
+
+    homeConfigurations.master = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [ ./home-manager/home.nix ];
+    };
+
+    packages.${system} = {
+      nixos-mama = self.nixosConfigurations.nixos-mama.config.system.build.toplevel;
+      nixos-master = self.nixosConfigurations.nixos-master.config.system.build.toplevel;
+    };
+  };
 }
